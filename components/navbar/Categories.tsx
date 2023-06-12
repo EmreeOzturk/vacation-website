3bc41b35118948b06977a663262a65d89040dc18
Emre <emreeozturk42@gmail.com>
2023-06-13 02:30:35 +0300

    selected categories map to url params

--- components/navbar/Categories.tsx
+++ components/navbar/Categories.tsx
@@ -5,36 +5,43 @@ import { MdHolidayVillage } from "react-icons/md";
 import { FaUmbrellaBeach, FaCity } from "react-icons/fa";
 import { GiCampingTent } from "react-icons/gi";
 import CategoryItem from "./CategoryItem";
+import { useSearchParams } from "next/navigation";
 
 export const categories = [
   {
     name: "Village",
     icon: MdHolidayVillage as IconType,
-    href: "?village",
+    href: "/?q=village",
   },
   {
     name: "City",
     icon: FaCity as IconType,
-    href: "?city",
+    href: "/?q=city",
   },
   {
     name: "Beach",
     icon: FaUmbrellaBeach as IconType,
-    href: "?beach",
+    href: "/?q=beach",
   },
   {
     name: "Camp",
     icon: GiCampingTent as IconType,
-    href: "?camp",
+    href: "/?q=camp",
   },
 ];
 
 const Categories = () => {
+  const queryParam = useSearchParams().get("q");
+
   return (
     <div className="flex items-centers justify-center gap-5">
       {
         categories.map((category, i) => (
-          <CategoryItem key={i} {...category} selected={false} />
+          <CategoryItem
+            key={i}
+            {...category}
+            selected={queryParam === category.name.toLowerCase()}
+          />
         )) as []
       }
     </div>
--- components/navbar/CategoryItem.tsx
+++ components/navbar/CategoryItem.tsx
@@ -1,18 +1,20 @@
 "use client";
 import { CategoryItemProps } from "@/types";
 import React from "react";
-
+import { useRouter } from "next/navigation";
 const CategoryItem: React.FC<CategoryItemProps> = ({
   name,
   icon: Icon,
   href,
   selected,
 }) => {
+  const router = useRouter();
   return (
     <div
-      className={`${selected && 'border-b-2 border-red-500'} flex items-center justify-center gap-1 text-xl hover:bg-red-200`
-        
-      }
+      onClick={() => router.push(href)}
+      className={`${
+        selected && "border-b-2 border-red-500"
+      } flex items-center justify-center gap-1 text-xl hover:bg-red-200`}
     >
       <Icon />
       {name}
